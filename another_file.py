22dc331d4fbbdf8d4ca2dd6ff396cd2e0341c4f1
Carlos Lopez <carlosandreslopez@gmail.com>
2022-02-20 15:29:15 -0500

    New feauture from a new branch

--- another_file.py
+++ another_file.py
@@ -1,1 +1,4 @@
 print('This is another file')
+print('This is a print added from a branch')
+print('This is a print added from a branch')
+print('This is a print added from a branch')
---
+++ not-tracket.py
@@ -0,0 +1,2 @@
+print('This is a file not tracked')
+print('This is a change')
